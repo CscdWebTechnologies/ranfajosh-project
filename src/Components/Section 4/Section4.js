a2c710009f584dfd26ae0779e9f72ca77bd32094
Randel Daketse <randeldaketse18@gmail.com>
2021-05-14 16:57:09 +0000

    Styles applied to side text in Section4

--- src/Components/Section 4/Section4.js
+++ src/Components/Section 4/Section4.js
@@ -6,9 +6,9 @@ function Section4(){
     return(
         <Container>
             <Row>
-                <Col md={{span:4}}>
+                <Col md={{span:5}}>
                     <Row>
-                        <Col md={{span:2}} className="svgStyle">
+                        <Col md={{span:3}} className="svgStyle">
                         <svg width="30" height="32" viewBox="0 0 30 32">
                             <g fill="none" fill-rule="evenodd">
                             <path fill="#D4EEFF" d="M10 32h10V10H10z"></path><path fill="#1752F0" d="M20 32h10V0H20z"></path>
@@ -16,13 +16,13 @@ function Section4(){
                             </g>
                         </svg>
                         </Col>
-                        <Col md={{span:9}}>
+                        <Col md={{span:8}}>
                             <p className="sideHeader">Manage your portfolio</p> 
-                           <p>Buy and sell popular digital currencies, keep track of them in the one place.</p>
+                           <p className="sideText">Buy and sell popular digital currencies, keep track of them in the one place.</p>
                         </Col>
                     </Row>
                     <Row>
-                        <Col md={{span:2}} className="svgStyle">
+                        <Col md={{span:3}} className="svgStyle">
                         <svg width="36" height="38" viewBox="0 0 36 38">
                             <g fill="none" fill-rule="evenodd">
                             <path d="M17.813 9.5a2.375 2.375 0 002.375-2.375v-4.75a2.375 2.375 0 00-4.75 0v4.75A2.375 2.375 0 0017.811 9.5M29.688 9.5a2.375 2.375 0 002.375-2.375v-4.75a2.375 2.375 0 00-4.75 0v4.75A2.375 2.375 0 0029.688 9.5" fill="#D4EEFF"></path>
@@ -32,13 +32,13 @@ function Section4(){
                             </g>
                         </svg>
                         </Col>
-                        <Col md={{span:9}}>
+                        <Col md={{span:8}}>
                         <p className="sideHeader">Recurring buys</p> 
-                           <p>Invest in cryptocurrency slowly over time by scheduling buys daily, weekly, or monthly.</p>
+                           <p className="sideText">Invest in cryptocurrency slowly over time by scheduling buys daily, weekly, or monthly.</p>
                         </Col>
                     </Row>
                     <Row>
-                        <Col md={{span:2}} className="svgStyle">
+                        <Col md={{span:3}} className="svgStyle">
                         <svg width="30" height="33" viewBox="0 0 30 33" fill="none">
                             <path d="M15 0C9.106 0 4.284 4.64 4.284 11.344V25.78h21.429V11.344C25.714 4.64 20.892 0 14.999 0z" fill="#D4EEFF"></path>
                             <path d="M30 12.375H0V33h30V12.375z" fill="#56B4FC"></path>
@@ -47,15 +47,15 @@ function Section4(){
                             <path d="M15 20.625c1.184 0 2.143-.923 2.143-2.063 0-1.139-.96-2.062-2.143-2.062-1.183 0-2.143.923-2.143 2.063 0 1.139.96 2.062 2.143 2.062z" fill="#fff">
                             </path></svg>
                         </Col>
-                        <Col md={{span:9}}>
+                        <Col md={{span:8}}>
                         <p className="sideHeader">Vault protection</p> 
-                           <p>For added security, store your funds in a vault with time delayed withdrawals.  </p>
+                           <p className="sideText" >For added security, store your funds in a vault with time delayed withdrawals.  </p>
 
                       
                         </Col>
                     </Row>
                     <Row>
-                        <Col md={{span:2}}  className="svgStyle">
+                        <Col md={{span:3}}  className="svgStyle">
                         <svg width="36" height="36" viewBox="0 0 36 36" fill="none">
                             <path d="M36 2.25H0v31.5h36V2.25z" fill="#D4EEFF"></path>
                             <path d="M28.125 36H7.875a1.128 1.128 0 01-1.125-1.125V1.125C6.75.506 7.256 0 7.875 0h20.25c.618 0 1.125.506 1.125 1.125v33.75c0 .619-.507 1.125-1.125 1.125z" fill="#56B4FC"></path><path d="M29.25 2.25H6.75v31.5h22.5V2.25z" fill="#1652F0"></path>
@@ -64,13 +64,13 @@ function Section4(){
                             <path d="M23.625 3.375h-11.25a1.128 1.128 0 01-1.124-1.125V1.125h13.5V2.25c0 .619-.507 1.125-1.126 1.125z" fill="#56B4FC"></path><path d="M15.75 32.063c0 .337.226.562.563.562h3.375c.338 0 .563-.225.563-.562 0-.338-.226-.563-.563-.563h-3.375c-.337 0-.562.225-.562.563z" fill="#fff">
                             </path></svg>
                         </Col>
-                        <Col md={{span:9}}>
+                        <Col md={{span:8}}>
                         <p className="sideHeader">Mobile apps</p> 
-                           <p>Stay on top of the markets with the Coinbase app for Android or iOS.</p>
+                           <p className="sideText" >Stay on top of the markets with the Coinbase app for Android or iOS.</p>
                         </Col>
                     </Row>   
                 </Col>
-                <Col sm={{span:8}} >
+                <Col sm={{span:7}} >
                     <img src={image} alt="user interface" className="imageUI"/>
                 </Col>
             </Row>
